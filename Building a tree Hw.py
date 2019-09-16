c647359986e4c7a0ad6f69b912cf59a37d62f5e0
MauriceKoh <54858943+MauriceKoh@users.noreply.github.com>
2019-09-16 15:36:39 -0400

    Add files via upload

---
+++ Building a tree Hw.py
@@ -0,0 +1,1 @@
+#HW Building a treedef main():	n = int(input("Please enter the number of branches:"))	for x in range(n):		print((" " * (n - x)),('#' * (2*x+1)))	print(n*" ", "#")main()
