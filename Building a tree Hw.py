6e872a38ad61a86627454c4bcce139380c675186
MauriceKoh <54858943+MauriceKoh@users.noreply.github.com>
2019-09-16 15:38:48 -0400

    Update Building a tree Hw.py

--- Building a tree Hw.py
+++ Building a tree Hw.py
@@ -1,1 +1,16 @@
-#HW Building a treedef main():	n = int(input("Please enter the number of branches:"))	for x in range(n):		print((" " * (n - x)),('#' * (2*x+1)))	print(n*" ", "#")main()
+#HW Building a tree
+
+def main():
+	n = int(input("Please enter the number of branches:"))
+	for x in range(n):
+		print((" " * (n - x)),('#' * (2*x+1)))
+	print(n*" ", "#")
+
+main()
+
+
+
+
+
+
+
